87703a8c259977f96d2034ea94f6da122db8807c
Nesus98 <jesusarroyo30.98@gmail.com>
2024-07-31 12:58:35 +0200

    first steps in frontend done (form to create projects)

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+/** Projects */
+export const projectSchema = z.object({
+    _id: z.string(),
+    projectName: z.string(),
+    clientName: z.string(),
+    description: z.string(),
+})
