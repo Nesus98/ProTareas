9a5dbcafe7e250c7052bd40ec8f650c195503d65
Nesus98 <jesusarroyo30.98@gmail.com>
2024-08-01 23:05:09 +0200

    toastify added

--- frontend/src/views/projects/CreateProjectView.tsx
+++ frontend/src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,13 @@
 import ProjectForm from "@/components/projects/ProjectForm";
 import { createProject } from "@/api/ProjectAPI";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ProjectFormData } from "@/types/index";
+import { toast } from "react-toastify";
 
 export default function CreateProjectView() {
+
+  const navigate = useNavigate()
 
   const initialValues : ProjectFormData= {
     projectName: "",
@@ -18,8 +21,11 @@ export default function CreateProjectView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForm = (data : ProjectFormData) => {
-    createProject(data)
+  const handleForm = async (formData : ProjectFormData) => {
+   const data =  await createProject(formData)
+   toast.success(data)
+    navigate('/')
+
   };
   return (
     <>
