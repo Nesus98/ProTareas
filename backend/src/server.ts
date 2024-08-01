d624329e0ce3c9d0f5ad504b2e8f1d601ba80b33
Nesus98 <jesusarroyo30.98@gmail.com>
2024-08-01 22:54:37 +0200

    cors working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import { corsConfig } from './config/cors'
+import cors from 'cors'
 
 // Carga las variables de entorno desde un archivo .env a process.env
 dotenv.config()
@@ -11,6 +13,8 @@ connectDB()
 
 // Crea una instancia de una aplicación Express
 const app = express()
+
+app.use(cors(corsConfig))
 
 //Leer valores json en el body
 app.use(express.json())
