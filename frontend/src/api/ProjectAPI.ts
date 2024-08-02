e81e70b762c4d3dd4708f3ec917fcef7a22e9401
Nesus98 <jesusarroyo30.98@gmail.com>
2024-08-02 11:25:55 +0200

    dashboard for projects added

--- frontend/src/api/ProjectAPI.ts
+++ frontend/src/api/ProjectAPI.ts
@@ -1,17 +1,30 @@
-import { ProjectFormData } from "@/types/index";
+import { ProjectFormData, dashboardProjectSchema } from "@/types/index";
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 
-export async function createProject(formData : ProjectFormData){
-    try {
-        const{data} = await api.post('/projects', formData)
-        return data
-        
-    } catch (error) {
-        if(isAxiosError(error) && error.response){
-            throw new Error (error.response.data.error);
-        }
-        
+export async function createProject(formData: ProjectFormData) {
+  try {
+    const { data } = await api.post("/projects", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api("/projects");
+    const response = dashboardProjectSchema.safeParse(data)
+
+    if(response.success){
+        return response.data
     }
     
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
 }
