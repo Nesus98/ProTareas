84057c811b7736761d0e80c2ad6e9176d8bf88c6
Nesus98 <jesusarroyo30.98@gmail.com>
2024-08-08 23:37:38 +0200

    login auth finisheds

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,7 +1,14 @@
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 export const hashPassword = async (password: string) => {
-          //Hashear Password
-          const salt = await bcrypt.genSalt(10);
-          return  await bcrypt.hash(password, salt);
-}
+  //Hashear Password
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+export const checkPassword = async (
+  enteredPassword: string,
+  storedHash: string
+) => {
+  return await bcrypt.compare(enteredPassword, storedHash);
+};
